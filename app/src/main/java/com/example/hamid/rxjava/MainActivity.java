6e5ec40268eec06effbd35556e67f2de0acedb24
hrg.developer <hrg.developer@gmail.com>
2019-02-13 01:26:10 +0330

    new toast

--- app/src/main/java/com/example/hamid/rxjava/MainActivity.java
+++ app/src/main/java/com/example/hamid/rxjava/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv_test=(TextView)findViewById(R.id.tv_test);
-
+        Toast.makeText(getApplicationContext(), "salam", Toast.LENGTH_SHORT).show();
          Observable<String> items = getObservable();
 
          Observer<String> whatToDo = getObserver();
